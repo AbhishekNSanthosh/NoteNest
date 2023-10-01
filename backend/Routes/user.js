ae33d2999fb216a56f282d6760f10808bf3d6ed2
Abhishek <you@example.com>
2023-10-01 23:53:45 +0530

    backend added3

---
+++ backend/Routes/user.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const router = express.Router()
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const Note = require('../Models/Note');
+
+router.post('/signup', async (req, res, next) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (validator.isEmpty(username) || validator.matches(username, /[./\[\]{}<>]/)) {
+        return res.status(400).json({
+            status: 'FAILURE',
+            error: 'Invalid username'
+        });
+    }
+
+    if (validator.isEmpty(password) || validator.matches(password, /[./\[\]{}<>]/)) {
+        return res.status(400).json({
+            status: 'FAILURE',
+            error: 'Invalid username'
+        });
+    }
+    try {
+        const userExist = await User.findOne({ username })
+        if (userExist) {
+            return res.status(400).json({
+                statusCode: 400,
+                status: 'FAILURE',
+                error: 'Username already exists'
+            });
+        }
+        bcrypt.hash(password, 12).then(hashedPassword => {
+            const user = new User({
+                username,
+                password: hashedPassword
+            });
+            user.save();
+        }).then(() => {
+            return res.status(201).json({
+                statusCode: 201,
+                status: 'SUCCESS',
+                message: "Please login to continue"
+            })
+        })
+    } catch (err) {
+        if (err) {
+            return res.status(500).json({
+                error: err
+            })
+        }
+        next(err);
+    }
+})
