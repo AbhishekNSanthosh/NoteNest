c411922b28b0f9026c8a41cd3a406a40cec23370
Abhiiofficial <abhisheksanthosofficial19@gmail.com>
2023-10-23 20:30:16 +0530

    frontend-updates1

---
+++ frontend/src/components/SearchField/SearchField.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './SearchField.module.css'
+
+const SearchField = () => {
+    return (
+        <div>SearchField</div>
+    )
+}
+
+export default SearchField
--- frontend/src/modules/Dashboard/components/TopNavBar/TopNavBar.jsx
+++ frontend/src/modules/Dashboard/components/TopNavBar/TopNavBar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import styles from './TopNavBar.module.css'
+import SearchField from '../../../../components/SearchField/SearchField'
 
 const TopNavBar = () => {
   return (
-    <div className={styles.topNavBar}>TopNavBar</div>
+    <div className={styles.topNavBar}>
+      <div className={styles.topLeft}>
+        <SearchField />
+      </div>
+      <div className={styles.topRight}>
+s
+      </div>
+    </div>
   )
 }
 
